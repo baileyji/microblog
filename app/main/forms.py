6afcbdbafd276d54fc18fb74b14922b5e2718ad5
Jeb Bailey <baileyji@ucsb.edu>
2021-08-21 23:02:07 -0700

    begin final sprint, start ripping out cruft

--- app/main/forms.py
+++ app/main/forms.py
@@ -16,51 +16,5 @@ class CloudControl(FlaskForm):
     muted = BooleanField()
 
 
-def generate_effect_form(effect_form):
-    class ModeForm(FlaskForm):
-        effect = FormField(effect_form, seperator='modefoo:')
-        submit = SubmitField("Activate!")
-        jazz = BooleanField(_l('Jazzify?'))
-
-    return ModeForm
-
-
-class EditProfileForm(FlaskForm):
-    username = StringField(_l('Username'), validators=[DataRequired()])
-    about_me = TextAreaField(_l('About me'), validators=[Length(min=0, max=140)])
-    submit = SubmitField(_l('Submit'))
-
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
-        self.original_username = original_username
-
-    def validate_username(self, username):
-        if username.data != self.original_username:
-            user = User.query.filter_by(username=self.username.data).first()
-            if user is not None:
-                raise ValidationError(_('Please use a different username.'))
-
-
 class EmptyForm(FlaskForm):
     submit = SubmitField('Submit')
-
-
-class PostForm(FlaskForm):
-    post = TextAreaField(_l('Say something'), validators=[DataRequired()])
-    submit = SubmitField(_l('Submit'))
-
-
-class SearchForm(FlaskForm):
-    q = StringField(_l('Search'), validators=[DataRequired()])
-
-    def __init__(self, *args, **kwargs):
-        if 'formdata' not in kwargs:
-            kwargs['formdata'] = request.args
-        if 'csrf_enabled' not in kwargs:
-            kwargs['csrf_enabled'] = False
-        super(SearchForm, self).__init__(*args, **kwargs)
-
-
-class MessageForm(FlaskForm):
-    message = TextAreaField(_l('Message'), validators=[DataRequired(), Length(min=1, max=140)])
-    submit = SubmitField(_l('Submit'))
--- app/main/routes.py
+++ app/main/routes.py
@@ -8,7 +8,7 @@ from flask_babel import _, get_locale
 
 import cloudlight.fadecandy
 from .. import db
-from .forms import EditProfileForm, EmptyForm, PostForm, SearchForm, MessageForm
+from .forms import EmptyForm
 from ..models import User, Post, Message, Notification
 from ..translate import translate
 from . import bp
@@ -34,6 +34,7 @@ def before_request():
     g.redis = current_app.redis
     g.mode = current_app.redis.read('lamp:mode')
 
+
 @bp.after_request
 def add_header(response):
     response.headers['Cache-Control'] = 'no-cache, no-store'
@@ -44,22 +45,44 @@ def add_header(response):
 @bp.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    form = PostForm()
-    if form.validate_on_submit():
-        language = guess_language(form.post.data)
-        if language == 'UNKNOWN' or len(language) > 5:
-            language = ''
-        post = Post(body=form.post.data, author=current_user, language=language)
-        db.session.add(post)
-        db.session.commit()
-        flash(_('Your post is now live!'))
-        return redirect(url_for('main.index'))
-    page = request.args.get('page', 1, type=int)
-    posts = current_user.followed_posts().paginate(page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
-    return render_template('index.html', title=_('Home'), form=form, posts=posts.items, next_url=next_url,
-                           prev_url=prev_url)
+    # form = PostForm()
+    # if form.validate_on_submit():
+    #     language = guess_language(form.post.data)
+    #     if language == 'UNKNOWN' or len(language) > 5:
+    #         language = ''
+    #     post = Post(body=form.post.data, author=current_user, language=language)
+    #     db.session.add(post)
+    #     db.session.commit()
+    #     flash(_('Your post is now live!'))
+    #     return redirect(url_for('main.index'))
+    # page = request.args.get('page', 1, type=int)
+    # posts = current_user.followed_posts().paginate(page, current_app.config['POSTS_PER_PAGE'], False)
+    # next_url = url_for('main.index', page=posts.next_num) if posts.has_next else None
+    # prev_url = url_for('main.index', page=posts.prev_num) if posts.has_prev else None
+    # return render_template('index.html', title=_('Home'), form=form, posts=posts.items, next_url=next_url,
+    #                        prev_url=prev_url)
+
+    from cloudlight.fadecandy import EFFECTS
+    modes = tuple((key, e.name) for key, e in cloudlight.fadecandy.EFFECTS.items())
+    if request.method == 'POST':
+        mode = request.form['mode_key']
+        e = EFFECTS[mode]
+        f = e.form(mode_name=e.name, mode_key=e.key)
+        if f.validate_on_submit():
+            settings = f.data
+            for k in ('csrf_token', 'mode_name', 'mode_key', 'submit'):
+                settings.pop(k)
+            g.redis.store(f'lamp:{mode}:settings', settings)
+            if g.redis.read('lamp:mode') == mode:
+                g.redis.store(f'lamp:settings', True, publish_only=True)
+            else:
+                g.redis.store(f'lamp:mode', mode)
+
+            return redirect(url_for('main.lamp'))
+    else:
+        f = cloudlight.fadecandy.build_form(g.redis.read('lamp:mode'), g.redis)
+
+    return render_template('index.html', title=_('Cloudlight'), modes=modes, form=f, active_mode=g.mode)
 
 
 @bp.route('/rediscontrol', methods=['POST', 'GET'])
@@ -84,18 +107,17 @@ def rediscontrol():
     return bad_request('control failed')
 
 
-
 # Controls need to be named with their redis key
-
-
-
 @bp.route('/stream',  methods=['GET'])
 @login_required
 def stream():
     from ....config import REDIS_SCHEMA
     import cloudlight.cloudredis as clr
+    event = request.args.get('event', 'update', type=str)
+
     # @copy_current_request_context
     def _stream():
+        # for k, v in g.redis.listen(REDIS_SCHEMA['keys']):
         for k, v in clr.redis.listen(REDIS_SCHEMA['keys']):
             event = 'update'
             data = {k: v}
@@ -106,10 +128,9 @@ def stream():
             # new = list(zip(*redis.range(plotid, since)))
             # data = {'id': f'redisplot:{plotid}', 'kind': kind, 'data': {'x': new[0], 'y': new[1]}}
 
-            msg = f"event:{event}\nretry:5\ndata: {json.dumps(data)}\n\n"
-            yield msg
-    return current_app.response_class(_stream(), mimetype="text/event-stream")
+            yield f"event:{event}\nretry:5\ndata: {json.dumps(data)}\n\n"
 
+    return current_app.response_class(_stream(), mimetype="text/event-stream")
 
 
 @bp.route('/status')
@@ -118,9 +139,9 @@ def status():
     from ....config import REDIS_SCHEMA
     table = [('Setting', 'Value')]
     table += [(k, k, v) for k, v in current_app.redis.read(REDIS_SCHEMA['keys']).items()]
-
-    times, vals = list(zip(*g.redis.range('temp:value_avg120000')))
-    times = np.array(times,dtype='datetime64[ms]')
+    from datetime import timedelta
+    times, vals = list(zip(*g.redis.range('temp:value_avg120000', start=datetime.now()-timedelta(days=1))))
+    times = np.array(times, dtype='datetime64[ms]')
     fig = px.line(x=times, y=vals, title='Temps')
     # TODO set data_revision based on time interval
     tempfig = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
@@ -128,30 +149,27 @@ def status():
     return render_template('status.html', title=_('Settings'), table=table, tempfig=tempfig)
 
 
-@bp.route('/lamp', methods=['GET', 'POST'])
+@bp.route('/settings')
 @login_required
-def lamp():
-    from cloudlight.fadecandy import EFFECTS
-    modes = tuple((key, e.name) for key, e in cloudlight.fadecandy.EFFECTS.items())
-    if request.method == 'POST':
-        mode = request.form['mode_key']
-        e = EFFECTS[mode]
-        f = e.form(mode_name=e.name, mode_key=e.key)
-        if f.validate_on_submit():
-            settings = f.data
-            for k in ('csrf_token', 'mode_name', 'mode_key', 'submit'):
-                settings.pop(k)
-            g.redis.store(f'lamp:{mode}:settings', settings)
-            if g.redis.read('lamp:mode') == mode:
-                g.redis.store(f'lamp:settings', True, publish_only=True)
-            else:
-                g.redis.store(f'lamp:mode', mode)
+def settings():
+    return render_template('settings.html', title=_('Settings'))
 
-            return redirect(url_for('main.lamp'))
-    else:
-        f = cloudlight.fadecandy.build_form(g.redis.read('lamp:mode'), g.redis)
 
-    return render_template('lamp.html', title=_('Lamp'), modes=modes, form=f, active_mode=g.mode)
+@bp.route('/off')
+@login_required
+def off():
+    return index()
+
+
+@bp.route('/help')
+@login_required
+def help():
+    return render_template('help.html', title=_('Help'))
+
+
+@bp.route('/pihole')
+def pihole():
+    return redirect('https://cloudlight.local/admin')
 
 
 @bp.route('/modeform', methods=['POST'])
@@ -165,107 +183,10 @@ def modeform():
         return bad_request(f'"{mode}" is not known')
 
 
-@bp.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    page = request.args.get('page', 1, type=int)
-    posts = user.posts.order_by(Post.timestamp.desc()).paginate(
-        page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.user', username=user.username,
-                       page=posts.next_num) if posts.has_next else None
-    prev_url = url_for('main.user', username=user.username,
-                       page=posts.prev_num) if posts.has_prev else None
-    form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts.items,
-                           next_url=next_url, prev_url=prev_url, form=form)
-
-
-
-
-@bp.route('/follow/<username>', methods=['POST'])
-@login_required
-def follow(username):
-    form = EmptyForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=username).first()
-        if user is None:
-            flash(_('User %(username)s not found.', username=username))
-            return redirect(url_for('main.index'))
-        if user == current_user:
-            flash(_('You cannot follow yourself!'))
-            return redirect(url_for('main.user', username=username))
-        current_user.follow(user)
-        db.session.commit()
-        flash(_('You are following %(username)s!', username=username))
-        return redirect(url_for('main.user', username=username))
-    else:
-        return redirect(url_for('main.index'))
-
-
-
-
-@bp.route('/translate', methods=['POST'])
-@login_required
-def translate_text():
-    return jsonify({'text': translate(request.form['text'],
-                                      request.form['source_language'],
-                                      request.form['dest_language'])})
-
-
-@bp.route('/search')
-@login_required
-def search():
-    if not g.search_form.validate():
-        return redirect(url_for('main.explore'))
-    page = request.args.get('page', 1, type=int)
-    posts, total = Post.search(g.search_form.q.data, page,
-                               current_app.config['POSTS_PER_PAGE'])
-    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
-        if total > page * current_app.config['POSTS_PER_PAGE'] else None
-    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
-        if page > 1 else None
-    return render_template('search.html', title=_('Search'), posts=posts,
-                           next_url=next_url, prev_url=prev_url)
-
-
-@bp.route('/messages')
-@login_required
-def messages():
-    current_user.last_message_read_time = datetime.utcnow()
-    current_user.add_notification('unread_message_count', 0)
-    db.session.commit()
-    page = request.args.get('page', 1, type=int)
-    messages = current_user.messages_received.order_by(
-        Message.timestamp.desc()).paginate(
-            page, current_app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('main.messages', page=messages.next_num) \
-        if messages.has_next else None
-    prev_url = url_for('main.messages', page=messages.prev_num) \
-        if messages.has_prev else None
-    return render_template('messages.html', messages=messages.items,
-                           next_url=next_url, prev_url=prev_url)
-
-
-@bp.route('/export_posts')
-@login_required
-def export_posts():
-    if current_user.get_task_in_progress('export_posts'):
-        flash(_('An export task is currently in progress'))
-    else:
-        current_user.launch_task('export_posts', _('Exporting posts...'))
-        db.session.commit()
-    return redirect(url_for('main.user', username=current_user.username))
-
-
 @bp.route('/notifications')
 @login_required
 def notifications():
     since = request.args.get('since', 0.0, type=float)
     notifications = current_user.notifications.filter(
         Notification.timestamp > since).order_by(Notification.timestamp.asc())
-    return jsonify([{
-        'name': n.name,
-        'data': n.get_data(),
-        'timestamp': n.timestamp}
-        for n in notifications])
+    return jsonify([{'name': n.name, 'data': n.get_data(), 'timestamp': n.timestamp} for n in notifications])
